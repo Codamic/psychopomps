fe1f2608b9e5b259f26f760e3f82dd1b60ff3488
Sameer Rahmani <lxsameer@gnu.org>
2017-04-10 02:33:53 +0430

    fetch function added to utils module to support both sync and async version

--- src/psychopomps/utils.clj
+++ src/psychopomps/utils.clj
@@ -1,9 +1,8 @@
 (ns psychopomps.utils
   "A namespace for utility functions"
-  (:require [clojure.core.async  :refer [put!]]
+  (:require [clojure.core.async  :refer [put!] :as async]
             [org.httpkit.client  :as client]
             [psychopomps.logger  :as logger]))
-
 
 (defmacro while-let
   "Repeatedly executes body while test expression is true, evaluating
@@ -14,15 +13,44 @@
        ~@body
        (recur ~test))))
 
+(defn- internal-fetch
+  [url options]
+  (let [{:keys [status headers body error] :as resp} @(client/get url options)]
+    (if error
+      (do (logger/error "Could not fetch the sources.")
+          "")
+      body)))
+
+(defn fetch
+  "Fetch the given url with the given options and return a channel which
+  contains the result"
+  ([url options]
+   (fetch url options true))
+  ([url options async?]
+   (if async?
+     (async/thread
+       (internal-fetch url options))
+     (internal-fetch url options))))
+
 (defn fetch>
-  "Fetch the given url data and and put it into `out` channel"
+  "Fetch the given url data and and put it into `out` channel. Any given
+  options will pass to `org.httpkit.client/get` function and any given
+  function as last parameter will applied to the result which is going
+  to go in `out` channel."
   ([out url]
    (fetch> out url {:timeout 2000}))
 
   ([out url options]
+   (fetch> out url options (fn [body] body)))
+
+  ([out url options f]
+   (logger/debug "%s - %s - %s - %s" out url options f)
    (client/get url options
                (fn [{:keys [status headers body error]}]
+                 (logger/fatal "<<<")
                  (if error
                    (do (logger/warn "Request failed to '%s' with '%s' status." url status)
                        (logger/warn "Body: %s" body))
-                   (put! out body))))))
+                   (do
+                     (logger/debug "Data fetched from '%s' with '%s'" url options)
+                     (put! out (f body))))))))
