33989fcc01b07f5a12a56ef98e3616f293e61adb
Sameer Rahmani <lxsameer@gnu.org>
2017-05-01 11:55:12 +0100

    Basic event router added.

--- src/psychopomps/system.clj
+++ src/psychopomps/system.clj
@@ -1,6 +1,7 @@
 (ns psychopomps.system
   "System defination of psychopomps."
   (:require
+   [hellhound.events                 :refer [defrouter]]
    [hellhound.components.webserver   :as webserver]
    [hellhound.components.websocket   :as websocket]
    [psychopomps.logger               :refer [new-logger]]
@@ -8,6 +9,11 @@
    [psychopomps.db                   :refer [new-cassandra-client]]
    [psychopomps.collectors.core      :refer [new-collector-pool]]
    [psychopomps.jobs.convertors.html :refer [new-html->md-job]]))
+
+
+
+(defrouter event-router
+  {:event1 (fn [data _] (println data))})
 
 (def dev
   {:components {:logger     {:record (new-logger)
@@ -17,13 +23,13 @@
                 ;;              :requires [:logger]
                 ;;              :started nil}
 
-                :websocket {:record (websocket/make-websocket)}
+                :websocket {:record (websocket/make-websocket event-router)}
                 :webserver {:record (webserver/make-webserver dev-handler)
                             :requires [:websocket]}
 
-                :collectors {:record (new-collector-pool)
-                             :requires [:logger]
-                             :started nil}
+                ;; :collectors {:record (new-collector-pool)
+                ;;              :requires [:logger]
+                ;;              :started nil}
 
                 ;; :html-to-md {:record (new-html->md-job)
                 ;;              :requires [:logger :collectors]
