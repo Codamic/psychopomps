aa819a588ab6d198add237feb783a854b68b2623
Sameer Rahmani <lxsameer@gnu.org>
2017-06-06 02:40:42 +0100

    migration lock added

--- src/psychopomps/system.clj
+++ src/psychopomps/system.clj
@@ -13,29 +13,31 @@
 
 
 (def dev-system
-  {:components {:logger     {:record (new-logger)
-                             :started nil}
+  {:components
+   {
+    :logger     {:record (new-logger)
+                 :started nil}
 
-                ;; This name should be the same as configuration key in the
-                ;; Application configuration under the `:db` keyboard
-                :cassandra  {:record (cassandra/make-cassandra-client)
-                             :requires [:logger]
-                             :started nil}
+    ;; This name should be the same as configuration key in the
+    ;; Application configuration under the `:db` keyboard
+    :cassandra  {:record (cassandra/make-cassandra-client)
+                 :requires [:logger]
+                 :started nil}
 
-                :websocket {:record (websocket/make-websocket message-router)}
-                :webserver {:record (webserver/make-webserver dev-handler)
-                            :requires [:websocket]}
+    :websocket {:record (websocket/make-websocket message-router)}
+    :webserver {:record (webserver/make-webserver dev-handler)
+                :requires [:websocket]}
 
-                ;; :collectors {:record (new-collector-pool)
-                ;;              :requires [:logger]
-                ;;              :started nil}
+    ;; :collectors {:record (new-collector-pool)
+    ;;              :requires [:logger]
+    ;;              :started nil}
 
-                ;; :cache-to-redis {:record (new-redis-cache)
-                ;;                  :requires [:collectors]
-                ;;                  :inputs [:collectors]}
+    ;; :cache-to-redis {:record (new-redis-cache)
+    ;;                  :requires [:collectors]
+    ;;                  :inputs [:collectors]}
 
-                ;; :html-to-md {:record (new-html->md-job)
-                ;;              :requires [:logger :collectors]
-                ;;              :started nil
-                ;;              :inputs [:collectors]}
-                }})
+    ;; :html-to-md {:record (new-html->md-job)
+    ;;              :requires [:logger :collectors]
+    ;;              :started nil
+    ;;              :inputs [:collectors]}
+    }})
